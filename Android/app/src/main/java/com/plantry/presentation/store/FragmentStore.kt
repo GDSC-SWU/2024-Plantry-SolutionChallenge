5947d7bcc50bd1aafd9b66e513e89b6a5e0b777c
yerim123456 <yearim1226@gmail.com>
2024-01-08 10:45:15 +0900

    feat: Add feature settings

---
+++ Android/app/src/main/java/com/plantry/presentation/store/FragmentStore.kt
@@ -0,0 +1,11 @@
+package com.plantry.presentation.store
+
+import com.plantry.R
+import com.plantry.coreui.base.BindingFragment
+import com.plantry.databinding.FragmentStoreBinding
+
+class FragmentStore : BindingFragment<FragmentStoreBinding>(R.layout.fragment_store) {
+    override fun initView() {
+    }
+
+}
