463d06fe77098dc308b00e7caf803c426f43aa44
yerim123456 <yearim1226@gmail.com>
2024-01-08 10:45:15 +0900

    feat: Add feature settings

---
+++ Android/app/src/main/java/com/plantry/presentation/home/FragmentHome.kt
@@ -0,0 +1,11 @@
+package com.plantry.presentation.home
+
+import com.plantry.R
+import com.plantry.coreui.base.BindingFragment
+import com.plantry.databinding.FragmentHomeBinding
+
+class FragmentHome  : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun initView() {
+    }
+
+}
