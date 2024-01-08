b6c977be5118b82d548dc3af42389a09fd7b6f26
Yusun Choi <nsuy.ch@gmail.com>
2024-01-08 15:47:52 +0900

    chore: Add initial application (PLTR-27)

---
+++ server/plantry/src/test/java/com/gdscplantry/plantry/PlantryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdscplantry.plantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
