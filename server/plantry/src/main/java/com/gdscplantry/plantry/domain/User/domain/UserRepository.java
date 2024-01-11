771405ae59a65369e4c13a57ec06e09f7e908916
Yusun Choi <nsuy.ch@gmail.com>
2024-01-12 01:54:03 +0900

    feat: Add `findByEmail` on UserRepository (PLTR-48)

--- server/plantry/src/main/java/com/gdscplantry/plantry/domain/User/domain/UserRepository.java
+++ server/plantry/src/main/java/com/gdscplantry/plantry/domain/User/domain/UserRepository.java
@@ -3,6 +3,9 @@ package com.gdscplantry.plantry.domain.User.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
