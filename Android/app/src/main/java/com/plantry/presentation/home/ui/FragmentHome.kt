cfa5c8a0d3a16cea5c5001a1cf7b868c5b419433
yerim123456 <yearim1226@gmail.com>
2024-01-14 17:28:43 +0900

    feat: Add home&login ui(PLTR-40)

---
+++ Android/app/src/main/java/com/plantry/presentation/home/ui/FragmentHome.kt
@@ -0,0 +1,66 @@
+package com.plantry.presentation.home.ui
+
+import android.view.View
+import androidx.fragment.app.DialogFragment.STYLE_NO_TITLE
+import com.plantry.R
+import com.plantry.coreui.base.BindingFragment
+import com.plantry.data.dto.ResponseHomeDto
+import com.plantry.databinding.FragmentHomeBinding
+import com.plantry.presentation.home.adapter.PantryAdapter
+import com.plantry.presentation.home.bottomsheet.HomeElseBottomSheet
+import com.plantry.presentation.home.popup.HomePlusPopUp
+
+class FragmentHome : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun initView() {
+        listSetting()
+    }
+
+    private fun listSetting() {
+        val adapter = PantryAdapter()
+        // 가짜 데이터
+        val pantryList = listOf(
+            ResponseHomeDto("pantry1", true, "y"),
+            ResponseHomeDto("pantry2", false, "y"),
+            ResponseHomeDto("pantry3", true, "r"),
+            ResponseHomeDto("", false, "")
+        )
+        clickItem(adapter, pantryList)
+        binding.rcvHomeRecyclerview.adapter = adapter
+        adapter.submitList(pantryList)
+    }
+
+    private fun clickItem(adapter: PantryAdapter, list: List<ResponseHomeDto>) {
+        adapter.itemClick = object : PantryAdapter.PantryItemClick {
+            override fun onClick(view: View, position: Int) {
+                when (view.id) {
+                    R.id.cl_home_item_layout -> {
+                        if(position.equals(list.size -1)){
+                            val pantryPlusPopUp = HomePlusPopUp()
+                            pantryPlusPopUp.setStyle(STYLE_NO_TITLE, R.style.Theme_Plantry_AlertDialog)
+                            pantryPlusPopUp.show(parentFragmentManager, POP_UP)
+                        }
+                        else{
+                            // 일반 pantry 화면 넘어가는 로직
+                        }
+                    }
+                    R.id.iv_home_item_else -> {
+                        val editAndDeleteBottomsheet = HomeElseBottomSheet()
+                        editAndDeleteBottomsheet.show(parentFragmentManager, BOTTOM_SHEET)
+                    }
+
+                    R.id.iv_home_item_heart -> {
+                        // 하트 클릭 로직
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val BOTTOM_SHEET = "home_else_bottom_sheet"
+        const val POP_UP = "home_plus_pop_up"
+    }
+}
+
+
