47ec9051e5c24466c1e5c4e81226d8e0d63467f7
yerim123456 <yearim1226@gmail.com>
2024-01-14 17:32:51 +0900

    feat: Add home delete popup(PLTR-40)

---
+++ Android/app/src/main/java/com/plantry/presentation/home/popup/HomeDeletePopUp.kt
@@ -0,0 +1,20 @@
+package com.plantry.presentation.home.popup
+
+import android.util.Log
+import com.plantry.R
+import com.plantry.coreui.base.BindingDialogFragment
+import com.plantry.databinding.BottomsheetHomeElseBinding
+import com.plantry.databinding.PopupHomeDeleteBinding
+
+class HomeDeletePopUp :
+    BindingDialogFragment<PopupHomeDeleteBinding>(R.layout.popup_home_delete) {
+    override fun initView() {
+        clickCancleButton()
+    }
+
+    private fun clickCancleButton(){
+        binding.tvHomeDeletePopupCancle.setOnClickListener {
+            dialog?.dismiss()
+        }
+    }
+}
