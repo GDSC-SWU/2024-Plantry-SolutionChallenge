d64245816d4fe2ad39faf4d1fe06df2ead1989f1
yerim123456 <yearim1226@gmail.com>
2024-01-15 05:43:44 +0900

    feat: Add home pantry ui(PLTR-40)

---
+++ Android/app/src/main/java/com/plantry/presentation/home/ui/FragmentHomePantry.kt
@@ -0,0 +1,11 @@
+package com.plantry.presentation.home.ui
+
+import com.plantry.R
+import com.plantry.coreui.base.BindingFragment
+import com.plantry.databinding.FragmentHomePantryBinding
+
+
+class FragmentHomePantry : BindingFragment<FragmentHomePantryBinding>(R.layout.fragment_home_pantry) {
+    override fun initView() {
+    }
+}
