7c2346ea995a6a026060293d072972188c18a476
yerim123456 <yearim1226@gmail.com>
2024-01-15 17:01:14 +0900

    feat: Add home&home_pantry action(PLTR-40)

--- Android/app/src/main/java/com/plantry/presentation/home/ui/FragmentHomePantry.kt
+++ Android/app/src/main/java/com/plantry/presentation/home/ui/FragmentHomePantry.kt
@@ -1,11 +1,32 @@
 package com.plantry.presentation.home.ui
 
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.plantry.R
 import com.plantry.coreui.base.BindingFragment
 import com.plantry.databinding.FragmentHomePantryBinding
 
 
-class FragmentHomePantry : BindingFragment<FragmentHomePantryBinding>(R.layout.fragment_home_pantry) {
+class FragmentHomePantry :
+    BindingFragment<FragmentHomePantryBinding>(R.layout.fragment_home_pantry) {
+
+    override var bnvVisibility = View.GONE
+
     override fun initView() {
+        setPantryName()
+        backToHome()
     }
+
+    private fun setPantryName() {
+        val pantry_name = arguments?.getString("pantry_name")
+        binding.tvHomePantryName.setText(pantry_name)
+    }
+
+    private fun backToHome(){
+        binding.ivHomePantryBack.setOnClickListener {
+            findNavController().navigate(R.id.action_home_pantry_to_home)
+        }
+    }
+
+
 }
