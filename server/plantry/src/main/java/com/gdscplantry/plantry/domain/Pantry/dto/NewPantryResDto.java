28971deb4ea11bbda9d5936c495a2a5b8d99372b
Yusun Choi <nsuy.ch@gmail.com>
2024-01-16 22:23:24 +0900

    feat: Add new pantry API (PLTR-58)

---
+++ server/plantry/src/main/java/com/gdscplantry/plantry/domain/Pantry/dto/NewPantryResDto.java
@@ -0,0 +1,21 @@
+package com.gdscplantry.plantry.domain.Pantry.dto;
+
+import com.gdscplantry.plantry.domain.Pantry.domain.UserPantry;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class NewPantryResDto {
+    private Long pantryId;
+    private String title;
+    private String color;
+
+    public NewPantryResDto(UserPantry userPantry) {
+        this.pantryId = userPantry.getPantryId();
+        this.title = userPantry.getTitle();
+        this.color = userPantry.getColor();
+    }
+}
