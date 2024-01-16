8ed5968723ce66b964bdcdffd5f10e5c1897c79c
Yusun Choi <nsuy.ch@gmail.com>
2024-01-16 23:14:31 +0900

    feat: Add pantry list API (PLTR-58)

---
+++ server/plantry/src/main/java/com/gdscplantry/plantry/domain/Pantry/dto/PantryListItemDto.java
@@ -0,0 +1,20 @@
+package com.gdscplantry.plantry.domain.Pantry.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class PantryListItemDto {
+    private Long id;
+    private String title;
+    private String color;
+    private Boolean isMarked;
+
+    public PantryListItemDto(Long id, String title, String color, Boolean isMarked) {
+        this.id = id;
+        this.title = title;
+        this.color = color;
+        this.isMarked = isMarked;
+    }
+}
