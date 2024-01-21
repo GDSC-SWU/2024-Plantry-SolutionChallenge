8571644b65da3e65032af2ba08d9c9057352564f
yerim123456 <yearim1226@gmail.com>
2024-01-21 11:21:50 +0900

    feat: Add api setting (PLTR-46)

---
+++ Android/app/src/main/java/com/plantry/data/Extension.kt
@@ -0,0 +1,4 @@
+package com.plantry.data
+
+fun String?.isJsonObject(): Boolean = this?.startsWith("{") == true && this.endsWith("}")
+fun String?.isJsonArray(): Boolean = this?.startsWith("[") == true && this.endsWith("]")
