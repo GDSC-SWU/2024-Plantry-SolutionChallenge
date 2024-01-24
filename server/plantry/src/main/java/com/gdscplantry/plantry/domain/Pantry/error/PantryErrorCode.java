4cc548b049acca4dc7eada86788449da2cfe23e5
Yusun Choi <nsuy.ch@gmail.com>
2024-01-24 23:35:47 +0900

    feat: Add product count update API (PLTR-64)

--- server/plantry/src/main/java/com/gdscplantry/plantry/domain/Pantry/error/PantryErrorCode.java
+++ server/plantry/src/main/java/com/gdscplantry/plantry/domain/Pantry/error/PantryErrorCode.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum PantryErrorCode implements ErrorCode {
+    INVALID_COUNT(HttpStatus.BAD_REQUEST, "Invalid count value."),
     PANTRY_ACCESS_DENIED(HttpStatus.FORBIDDEN, "Pantry access denied."),
     PRODUCT_ACCESS_DENIED(HttpStatus.FORBIDDEN, "Product access denied."),
     PANTRY_NOT_FOUND(HttpStatus.NOT_FOUND, "Pantry not found. (Invalid pantry id.)"),
