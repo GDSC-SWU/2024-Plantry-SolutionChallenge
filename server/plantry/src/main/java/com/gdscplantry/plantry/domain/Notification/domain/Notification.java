26eb0be1b497138cc13b4dfd6f0446ad228a2553
Yusun Choi <nsuy.ch@gmail.com>
2024-01-29 03:47:11 +0900

    feat: Add Notification entity (PLTR-83)

---
+++ server/plantry/src/main/java/com/gdscplantry/plantry/domain/Notification/domain/Notification.java
@@ -0,0 +1,77 @@
+package com.gdscplantry.plantry.domain.Notification.domain;
+
+import com.gdscplantry.plantry.domain.User.domain.User;
+import com.gdscplantry.plantry.domain.model.NotificationTypeEnum;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Notification")
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@DynamicUpdate
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationTypeEnum type;
+
+    private String title;
+
+    private String body;
+
+    private Long entityId;
+
+    private Boolean isChecked;
+
+    private LocalDateTime notifiedAt;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Notification(User user, NotificationTypeEnum type, String title, String body, Long entityId, LocalDateTime notifiedAt) {
+        this.user = user;
+        this.type = type;
+        this.title = title;
+        this.body = body;
+        this.entityId = entityId;
+        this.isChecked = false;
+        this.notifiedAt = notifiedAt;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Notification notification)) return false;
+
+        return Objects.equals(this.id, notification.getId()) &&
+                Objects.equals(this.user, notification.getUser()) &&
+                Objects.equals(this.type, notification.getType()) &&
+                Objects.equals(this.entityId, notification.getEntityId()) &&
+                Objects.equals(this.notifiedAt, notification.getNotifiedAt());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, type, entityId, notifiedAt);
+    }
+}
