10796a63192bf561490b116c1c2b5e5ddebfe6c2
Yusun Choi <nsuy.ch@gmail.com>
2024-02-03 03:54:59 +0900

    feat: Add message send on FcmUtil (PLTR-83)

--- server/plantry/src/main/java/com/gdscplantry/plantry/global/util/FcmUtil.java
+++ server/plantry/src/main/java/com/gdscplantry/plantry/global/util/FcmUtil.java
@@ -1,12 +1,72 @@
 package com.gdscplantry.plantry.global.util;
 
+import com.gdscplantry.plantry.domain.Notification.vo.MessageVo;
+import com.gdscplantry.plantry.global.error.GlobalErrorCode;
+import com.gdscplantry.plantry.global.error.exception.AppException;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class FcmUtil {
-    public void validateToken(String deviceToken) {
+    private final FirebaseMessaging firebaseMessaging;
 
+    public void validateToken(String deviceToken) {
+        // Validate FCM device token
+        RestTemplate restTemplate = new RestTemplate();
+    }
+
+    public void sendMessage(ArrayList<MessageVo> vos) {
+        ArrayList<Message> messageList = new ArrayList<>();
+        StringBuilder userList = new StringBuilder();
+
+        for (MessageVo vo : vos) {
+            Notification notification = Notification.builder()
+                    .setTitle(vo.getTitle())
+                    .setBody(vo.getBody())
+                    .build();
+
+            if (vo.getDeviceToken() == null) {
+                log.error("FCM Token not found. - User " + vo.getUserId());
+                continue;
+            }
+
+            messageList.add(Message.builder()
+                    .setToken(vo.getDeviceToken())
+                    .setNotification(notification)
+                    .build()
+            );
+
+            userList.append(vo.getUserId()).append(" ");
+        }
+
+        try {
+            if (messageList.isEmpty()) {
+                log.info("[FCM] Sending completed successfully. " + LocalDateTime.now());
+                log.info("*** target empty");
+                return;
+            }
+
+            firebaseMessaging.sendEach(messageList);
+
+            log.info("[FCM] Sending completed successfully. " + LocalDateTime.now());
+            log.info("*** " + userList);
+        } catch (FirebaseMessagingException e) {
+            log.error(e.getMessage());
+            throw new AppException(GlobalErrorCode.FCM_SEND_FAILED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new AppException(GlobalErrorCode.FCM_SEND_FAILED);
+        }
     }
 }
