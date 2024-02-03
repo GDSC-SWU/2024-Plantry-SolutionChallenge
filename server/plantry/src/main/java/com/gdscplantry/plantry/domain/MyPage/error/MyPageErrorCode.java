a71315650ef2d817667cfb68dfb58aec0f893b7e
Yusun Choi <nsuy.ch@gmail.com>
2024-02-04 02:33:48 +0900

    feat: Add Terms read API (PLTR-83)

--- server/plantry/src/main/java/com/gdscplantry/plantry/domain/MyPage/error/MyPageErrorCode.java
+++ server/plantry/src/main/java/com/gdscplantry/plantry/domain/MyPage/error/MyPageErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum MyPageErrorCode implements ErrorCode {
     NICKNAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "Nickname already exists."),
-    ;
+    INVALID_TERM_TYPE(HttpStatus.BAD_REQUEST, "Invalid type. (all / use / privacy)");
 
     private final HttpStatus httpStatus;
     private final String message;
