fc9ea9bcbfc47c6a9c4f095cab611999c3d333ca
Yusun Choi <nsuy.ch@gmail.com>
2024-02-03 16:43:01 +0900

    feat: Add nickname update API (PLTR-83)

--- server/plantry/src/main/java/com/gdscplantry/plantry/domain/MyPage/controller/MyPageController.java
+++ server/plantry/src/main/java/com/gdscplantry/plantry/domain/MyPage/controller/MyPageController.java
@@ -1,17 +1,18 @@
 package com.gdscplantry.plantry.domain.MyPage.controller;
 
+import com.gdscplantry.plantry.domain.MyPage.dto.UpdateNicknameResDto;
 import com.gdscplantry.plantry.domain.MyPage.dto.UserProfileResDto;
 import com.gdscplantry.plantry.domain.MyPage.service.MyPageService;
 import com.gdscplantry.plantry.domain.User.domain.User;
 import com.gdscplantry.plantry.global.common.DataResponseDto;
 import com.gdscplantry.plantry.global.common.ResponseDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -25,5 +26,13 @@ public class MyPageController {
         UserProfileResDto userProfileResDto = myPageService.getUserProfile(user);
 
         return ResponseEntity.ok(DataResponseDto.of(userProfileResDto, 200));
+    }
+
+    @PatchMapping("/nickname")
+    public ResponseEntity<ResponseDto> updateNickname(@RequestParam @Valid @Size(min = 2, max = 12) @NotBlank String nickname,
+                                                      @RequestAttribute User user) {
+        UpdateNicknameResDto updateNicknameResDto = myPageService.updateNickname(user, nickname);
+
+        return ResponseEntity.status(201).body(DataResponseDto.of(updateNicknameResDto, 201));
     }
 }
