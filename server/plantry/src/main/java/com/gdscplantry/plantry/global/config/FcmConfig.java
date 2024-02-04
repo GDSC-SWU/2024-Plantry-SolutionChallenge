0bc4a2a04bfe2bfdefc252e7e4d021da9e0ce83c
Yusun Choi <nsuy.ch@gmail.com>
2024-02-05 01:29:34 +0900

    fix: Modify fcm config to read json in container env (PLTR-83)

--- server/plantry/src/main/java/com/gdscplantry/plantry/global/config/FcmConfig.java
+++ server/plantry/src/main/java/com/gdscplantry/plantry/global/config/FcmConfig.java
@@ -7,10 +7,12 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Configuration
@@ -18,9 +20,13 @@ import java.util.List;
 public class FcmConfig {
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
-        ClassPathResource resource = new ClassPathResource("firebase/firebase-service-account-file.json");
+        // For prod
+        Path filePath = Paths.get("/firebase/firebase_account_file.json");
+        InputStream refreshToken = Files.newInputStream(filePath);
 
-        InputStream refreshToken = resource.getInputStream();
+        // For local
+        // ClassPathResource resource = new ClassPathResource("/firebase/firebase_account_file.json");
+        // InputStream refreshToken = resource.getInputStream();
 
         FirebaseApp firebaseApp = null;
         List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
