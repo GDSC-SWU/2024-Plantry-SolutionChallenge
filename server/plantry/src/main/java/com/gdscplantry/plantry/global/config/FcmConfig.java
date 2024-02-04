9358865cdfc2703e41c7b064b6fd9179e8f67b69
Yusun Choi <nsuy.ch@gmail.com>
2024-02-05 02:03:28 +0900

    fix: Add environment check on redis&fcm config (PLTR-90)

--- server/plantry/src/main/java/com/gdscplantry/plantry/global/config/FcmConfig.java
+++ server/plantry/src/main/java/com/gdscplantry/plantry/global/config/FcmConfig.java
@@ -5,8 +5,10 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,15 +20,22 @@ import java.util.List;
 @Configuration
 @Slf4j
 public class FcmConfig {
+    @Value("${PROFILE}")
+    private String profile;
+
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
-        // For prod
-        Path filePath = Paths.get("/firebase/firebase_account_file.json");
-        InputStream refreshToken = Files.newInputStream(filePath);
-
-        // For local
-        // ClassPathResource resource = new ClassPathResource("/firebase/firebase_account_file.json");
-        // InputStream refreshToken = resource.getInputStream();
+        String JSON_PATH = "/firebase/firebase_account_file.json";
+        InputStream refreshToken;
+        if (profile.contains("local")) {
+            // For local
+            ClassPathResource resource = new ClassPathResource(JSON_PATH);
+            refreshToken = resource.getInputStream();
+        } else {
+            // For prod
+            Path filePath = Paths.get(JSON_PATH);
+            refreshToken = Files.newInputStream(filePath);
+        }
 
         FirebaseApp firebaseApp = null;
         List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
