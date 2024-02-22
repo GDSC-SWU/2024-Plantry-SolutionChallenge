571ec4451a86624450856a2bed122955c30b5fa6
Yusun Choi <nsuy.ch@gmail.com>
2024-02-22 23:55:11 +0900

    refactor: Rename dto to `NotificationPermissionResDto` (PLTR-94)

--- server/plantry/src/main/java/com/gdscplantry/plantry/domain/MyPage/dto/NotificationPermissionResDto.java
+++ server/plantry/src/main/java/com/gdscplantry/plantry/domain/MyPage/dto/NotificationPermissionResDto.java
@@ -5,6 +5,6 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class UpdateNotificationPermissionResDto {
+public class NotificationPermissionResDto {
     private Boolean isPermitted;
 }
